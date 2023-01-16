5b1788c63a7cdee7c4dfcd7921c37905a1a66da1
Griffin Floto <gfloto@cedar1.cedar.computecanada.ca>
2023-01-16 10:25:33 -0800

    training scripts

--- scripts/baseline.sh
+++ scripts/baseline.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH --job-name=bug_test
+#SBATCH --job-name=unsup
 #SBATCH --account=def-ssanner
 #SBATCH --nodes=1
 #SBATCH --gpus-per-node=v100l:4
-#SBATCH --mem-per-gpu=8G
-#SBATCH --time=00-00:02:00
+#SBATCH --mem-per-gpu=24G
+#SBATCH --time=03-00:00:00
 #SBATCH --cpus-per-task=4
 export OMP_NUM_THREADS=16
 
@@ -14,24 +14,24 @@ cd /home/gfloto/projects/def-ssanner/gfloto/projects/text_diff/scripts
 module load gcc/9.3.0 arrow cuda/11
 
 python -m torch.distributed.launch \
---nproc_per_node=1 \
+--nproc_per_node=4 \
 --use_env run_train.py \
---name dtest \
+--name unsup \
 --dataset_unsup wiki \
 --folder_name /home/gfloto/scratch/diffusion_models \
 --data_dir /home/gfloto/scratch/datasets/detox \
 --diff_steps 2000 \
 --lr 0.0001 \
---learning_steps 100 \
---save_interval 1 \
+--learning_steps 400000 \
+--save_interval 10000 \
 --seed 102 \
 --noise_schedule sqrt \
 --hidden_dim 128 \
 --bsz 128 \
---microbatch 16 \
+--microbatch 32 \
 --dataset detox \
 --vocab bert \
 --seq_len 64 \
 --schedule_sampler lossaware \
---notes detox \
-#--resume_checkpoint /home/gfloto/scratch/diffusion_models/qqp/model000999.pt
+--notes detox
+#--resume_checkpoint /home/gfloto/scratch/diffusion_models/check_models/qqp_model000999.pt \
